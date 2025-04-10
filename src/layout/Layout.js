95a87262643db10267ff14058deadd4aa7813aa7
Abinash lenka <abinashlenkacool@gmail.com>
2025-04-10 20:44:53 +0530

    uoad

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,12 +1,13 @@
-import { cn } from '@/Utilities/cn'
+import { cn } from '@/Utilities/cn';
 import HeaderRightSection from '@/modules/HeaderRightSection';
-import Navbar from '@/modules/Navbar'
+import Navbar from '@/modules/Navbar';
 import ProfileModal from '@/modules/ProfileModal';
 import SocialFooter from '@/modules/SocialFooter';
 import { getUserDetail } from '@/services/APIs/helper';
 import { getUser } from '@/services/firebase-services/cookies';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+import NotificationBell from '@/modules/NotificationBell';
 
 export default function Layout({ children, className, role }) {
   return (
@@ -14,13 +15,18 @@ export default function Layout({ children, className, role }) {
       <div className={cn('wrapper flex flex-wrap items-start', className)}>
         <Navbar role={role} />
         <div className='w-full flex flex-wrap items-start h-full relative bg-gray-100'>
-          {/* <div className='w-full h-full absolute top-0 left-0 bg-landing opacity-40'></div> */}
-          <div className='w-full mt-8 px-8'>
+          {/* ✅ Notification Bell Top Right */}
+          <div className='w-full flex justify-end items-center px-8 pt-4'>
+            <NotificationBell role={role} />
+          </div>
+
+          {/* ✅ Page Content */}
+          <div className='w-full mt-4 px-8'>
             {children}
             <SocialFooter />
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
